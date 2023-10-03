b8cda42887ba9cc93054c5e3bbb8a391e9447c8d
emilianscheel <emilian-scheel@t-online.de>
2023-10-03 08:48:44 +0200

    build step; version 0.0.20

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -367,7 +367,7 @@ function OptionFieldHorizontal({ title, iconName, preselected = false }) {
         React.createElement("h3", null, title)));
 }
 
-var css_248z$a = ".styles-module_container__oNbrP {\n  padding-top: 10px;\n}\n.styles-module_container__oNbrP .styles-module_button__UBk65 {\n  padding: 10px 18px;\n  background: #0075FF;\n  border-radius: 40px;\n  cursor: pointer;\n  display: flex;\n  flex-direction: row;\n  gap: 10px;\n  font-size: 15px;\n  color: white;\n  align-items: center;\n  border: none;\n}\n.styles-module_container__oNbrP .styles-module_button__UBk65.styles-module_red__CHF3e {\n  background: #e40000;\n}";
+var css_248z$a = ".styles-module_container__oNbrP .styles-module_button__UBk65 {\n  padding: 10px 18px;\n  background: #0075FF;\n  border-radius: 40px;\n  cursor: pointer;\n  display: flex;\n  flex-direction: row;\n  gap: 10px;\n  font-size: 15px;\n  color: white;\n  align-items: center;\n  border: none;\n}\n.styles-module_container__oNbrP .styles-module_button__UBk65.styles-module_red__CHF3e {\n  background: #e40000;\n}";
 var styles$a = {"container":"styles-module_container__oNbrP","button":"styles-module_button__UBk65","red":"styles-module_red__CHF3e"};
 styleInject(css_248z$a);
 
