1233132bf67ca684513084d9a653acdd44c33eda
emilianscheel <emilian-scheel@t-online.de>
2023-10-19 20:12:49 +0200

    adds property 'onClick' to OptionFieldHorizontal

--- src/components/OptionFieldHorizontal/OptionFieldHorizontal.tsx
+++ src/components/OptionFieldHorizontal/OptionFieldHorizontal.tsx
@@ -5,14 +5,22 @@ import styles from './styles.module.scss'
 export function OptionFieldHorizontal({
     title,
     iconName,
+    onClick,
     preselected = false
 }: {
     title: string,
     iconName?: string,
+    onClick?: (event: any) => void,
     preselected?: boolean
 }) {
+
+    function onClickDiv(event: any) {
+        event.preventDefault()
+        if (onClick) onClick(event)
+    }
+
     return (
-        <div className={[styles.container, preselected ? styles.preselected : styles.not_preselected].join(" ")}>
+        <div className={[styles.container, preselected ? styles.preselected : styles.not_preselected].join(" ")} onClick={onClickDiv}>
             {iconName && <SvgIcon iconName={iconName} size="32px" />}
             <h3>{title}</h3>
         </div>
