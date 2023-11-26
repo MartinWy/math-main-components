ed8b6e488cfe9d5f3f567dda5a4c26a32dff763f
emilianscheel <emilian-scheel@t-online.de>
2023-11-26 10:57:20 +0100

    adds type Log

---
+++ src/types/Log.ts
@@ -0,0 +1,11 @@
+
+
+export interface Log {
+    id: string
+    title: string
+    message: string
+    httpCode?: number
+    stack?: string
+    application: "math-main-app" | "math-main-manager"
+    createdAt: Date
+}
