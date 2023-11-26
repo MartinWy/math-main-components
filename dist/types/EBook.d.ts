22e45da106d3bd36f21e52066c7c84444a9f741e
emilianscheel <emilian-scheel@t-online.de>
2023-11-26 12:32:32 +0100

    adds type EBook and Movie

---
+++ dist/types/EBook.d.ts
@@ -0,0 +1,19 @@
+import { PortableTextBlock } from "sanity";
+import { Category } from "./Category";
+export interface Movie {
+    _id: string;
+    title: string;
+    previewImage: {
+        asset: {
+            _ref: string;
+            _type: string;
+        };
+    };
+    author: string;
+    amountOfPages: number;
+    fileId: string;
+    fileExpire?: string;
+    fileUrl?: string;
+    description: PortableTextBlock[];
+    categories: Category[];
+}
