f522c0863663f05bf0f2214b6d544264e6c8cfdb
emilianscheel <emilian-scheel@t-online.de>
2023-12-09 13:32:00 +0100

    adds type Feedback

---
+++ dist/types/Feedback.d.ts
@@ -0,0 +1,10 @@
+import { User } from "./User";
+export interface Feedback {
+    id: string;
+    user: User;
+    userId: number;
+    message: string;
+    answers: object[];
+    createdAt: Date;
+    updatedAt: Date;
+}
